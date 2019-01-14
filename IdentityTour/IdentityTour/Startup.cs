0e25220d9be6c4bfa8d2d91be38a81d8d4494d2e
Jean Roux <jeanp.roux@gmail.com>
2019-01-14 11:47:49 +0200

    Initial commit

---
+++ IdentityTour/IdentityTour/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IdentityTour.Startup))]
+namespace IdentityTour
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
